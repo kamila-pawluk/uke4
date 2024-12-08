ad17b80b47e3f47ef49b98247adf6b6e40731427
kamila-pawluk <kamilapawluk.java@gmail.com>
2024-12-09 00:24:53 +0100

    finalVersion

--- reactionTime/controller.js
+++ reactionTime/controller.js
@@ -1,6 +1,20 @@
+let startTime;
 
-function turnOnTheLight(selectedLampIndex){
-    let random = Math.floor(Math.random * 24);
-    selectedLampIndex = random;
+function turnOnTheLight() {
+    model.selectedLampIndex = Math.floor(Math.random() * 25);
+    startTime = new Date().getTime();
+    updateView(model.selectedLampIndex, 'turnOffLight');
+}
 
+function turnOffLight(){
+    const finishTime = new Date().getTime();
+    const spentMilliseconds = finishTime - startTime;
+    const spentSeconds = spentMilliseconds / 1000;
+
+    alert(`Reaction time: ${spentSeconds} sec.`);
+
+   model.selectedLampIndex = null;
+   updateView(model.selectedLampIndex, '');
+
+   setTimeout(turnOnTheLight, 1000);
 }
--- reactionTime/view.js
+++ reactionTime/view.js
@@ -1,13 +1,32 @@
-let array = model.app.array;
-let array5 = array.slice(0,4);
-
-
-function updateView(){
+// function updateView(selectedLampIndex, onClickHandler){
+//     let app = document.getElementById('app');
+//     app.innerHTML = /*HTML*/ `
+//         <div id="circles" class="box"></div>
+//     `;
     
+//     let circles = '';
+//     for(let i = 0; i < 25; i++){
+//         const lightClass = i === selectedLampIndex ? 'lightOn' : '';
+//         const clickHandler = i === selectedLampIndex ? `onclick="${onClickHandler}()"` : '';
+    
+//         circles += `<div id="circle${i}" class="lamp ${lightClass}" ${clickHandler}></div>`;
+//     }
+    
+//     document.getElementById('circles').innerHTML = circles;
+//     }
 
+
+function updateView(selectedIndex, onClickHandler) {
+    let app = document.getElementById('app');
     app.innerHTML = /*HTML*/ `
-    <div> 
-    <div> @{array[0]} </div>
-    </div>
-    `
+        <div id="circles" class="box"></div>
+    `;
+    
+    let circles = '';
+    for (let i = 0; i < 25; i++) {
+        const lightClass = i === selectedIndex ? 'lightOn' : '';
+        const clickHandler = i === selectedIndex ? `onclick="${onClickHandler}()"` : '';
+        circles += `<div id="circle${i}" class="lamp ${lightClass}" ${clickHandler}></div>`; 
+    }
+    document.getElementById('circles').innerHTML = circles;
 }
